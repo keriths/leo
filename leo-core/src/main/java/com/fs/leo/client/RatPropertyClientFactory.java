8aefcb196b56335adb86d83a09368d41c225eb0f
shuai.fan <shuai.fan@dianping.com>
2016-06-22 18:55:13 +0800

    aaa

---
+++ leo-core/src/main/java/com/fs/leo/client/RatPropertyClientFactory.java
@@ -0,0 +1,38 @@
+package com.fs.leo.client;
+
+import com.fs.leo.exceptions.ZKClientInitException;
+import com.fs.leo.zk.RatZKClient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by fanshuai on 15/6/2.
+ */
+public class RatPropertyClientFactory {
+    private static RatPropertyClient defaultClient;
+    private static Map<String,RatPropertyClient> zkRatClientMap = new HashMap<String, RatPropertyClient>();
+    public static RatPropertyClient getDefaultRatClient() throws ZKClientInitException {
+        if (defaultClient == null){
+            synchronized (RatPropertyClientFactory.class){
+                if(defaultClient == null){
+                    defaultClient = new RatZKClient();
+                }
+            }
+        }
+        return defaultClient;
+    }
+    public static RatPropertyClient getZKRatClient(String zkClusterHost) throws ZKClientInitException {
+        RatPropertyClient ratPropertyClient = zkRatClientMap.get(zkClusterHost);
+        if(ratPropertyClient==null){
+            synchronized (zkClusterHost){
+                ratPropertyClient = zkRatClientMap.get(zkClusterHost);
+                if(ratPropertyClient==null){
+                    ratPropertyClient = new RatZKClient(zkClusterHost);
+                    zkRatClientMap.put(zkClusterHost,ratPropertyClient);
+                }
+            }
+        }
+        return ratPropertyClient;
+    }
+}
